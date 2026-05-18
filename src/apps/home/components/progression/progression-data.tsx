e5a6024d18c3c4bac5bb5e0036beaadab3e44f69
jailsonds27 <jailsondesouza273@gmail.com>
2026-05-17 22:55:44 -0300

    remove espace name

--- src/apps/home/components/progression/progression-data.tsx
+++ src/apps/home/components/progression/progression-data.tsx
@@ -7,77 +7,77 @@ export type ProgressionData = {
 export const PROGRESSIONS: ProgressionData[] = [
   {
     degrees: [1, 5, 6, 4],
-    name: "I - V - VI - IV",
+    name: "I-V-VI-IV",
     usedIn: "Pop, Rock, Sertanejo Universitário, Forró das Antigas",
   },
   {
     degrees: [1, 6, 4, 5],
-    name: "I - VI - IV - V",
+    name: "I-VI-IV-V",
     usedIn: "Anos 50/60, Pop Antigo, Jovem Guarda, Baladas",
   },
   {
     degrees: [2, 5, 1],
-    name: "II - V - I",
+    name: "II-V-I",
     usedIn: "Bossa Nova, Jazz, MPB, Pop Fino",
   },
   {
     degrees: [1, 4, 5],
-    name: "I - IV - V",
+    name: "I-IV-V",
     usedIn: "Rock Clássico, Blues, Sertanejo Raiz, Reggae",
   },
   {
     degrees: [6, 4, 1, 5],
-    name: "VI - IV - I - V",
+    name: "VI-IV-I-V",
     usedIn: "Pop Melancólico, Rock Alternativo, Sertanejo Pop",
   },
   {
     degrees: [1, 5, 4],
-    name: "I - V - IV",
+    name: "I-V-IV",
     usedIn: "Folk, Rock Nacional, Pop Rock",
   },
   {
     degrees: [6, 5, 4, 5],
-    name: "VI - V - IV - V",
+    name: "VI-V-IV-V",
     usedIn: "Flamenco, Música Latina, Pop, Pisadinha",
   },
   {
     degrees: [1, 3, 4, 5],
-    name: "I - III - IV - V",
+    name: "I-III-IV-V",
     usedIn: "Gospel, Baladas Românticas, MPB",
   },
   {
     degrees: [1, 2, 4, 1],
-    name: "I - II - IV - I",
+    name: "I-II-IV-I",
     usedIn: "MPB, Indie Rock, Folk",
   },
   {
     degrees: [1, 5, 6, 3, 4, 1, 4, 5],
-    name: "I - V - VI - III - IV - I - IV - V",
+    name: "I-V-VI-III-IV-I-IV-V",
     usedIn: "Música Clássica, Casamentos, Baladas Pop, Gospel",
   },
   {
     degrees: [1, 6, 2, 5],
-    name: "I - VI - II - V",
+    name: "I-VI-II-V",
     usedIn: "Jazz, Bossa Nova, Samba, Pop Tradicional",
   },
   {
     degrees: [6, 7, 1],
-    name: "BVI - BVII - I",
+    name: "VI-VII-I",
     usedIn: "Heavy Metal, Rock, Música Épica, Pop Moderno",
   },
   {
     degrees: [1, 7, 6, 5],
-    name: "I - VII - VI - V",
+    name: "I-VII-VI-V",
     usedIn: "Flamenco, Pop Latino, MPB, Rock Clássico",
   },
   {
     degrees: [4, 5, 3, 6],
-    name: "IV - V - III - VI",
+    name: "IV-V-III-VI",
     usedIn: "Pop Japonês (J-Pop), Anime Songs, Lo-Fi, R&B",
   },
   {
     degrees: [1, 4, 1, 5],
-    name: "I - IV - I - V",
+    name: "I-IV-I-V",
     usedIn: "Country, Sertanejo Raiz, Modão, Pagode",
   },
 ]
